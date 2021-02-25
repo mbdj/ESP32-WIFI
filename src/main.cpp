2b5238a571c48ed892158daf54198143d97faeae
mehdi <mbdj@free.fr>
2021-02-25 11:12:28 +0100

    connexion au wifi ok

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,28 @@ Mehdi Ben Djedidia 25/02/2021
 *****************************/
 
 #include <Arduino.h>
+#include <WiFi.h>
+#include <secret.h>
+
+const char *SSID{SSID_SECRET};
+const char *PASSWD{PASSWD_SECRET};
+const uint8_t PIN_OUTPUT{5};
 
 void setup()
 {
+  Serial.begin(9600L);
+  pinMode(PIN_OUTPUT, OUTPUT);
+  WiFi.mode(WIFI_STA);
 
-
+  digitalWrite(PIN_OUTPUT, LOW);
+  WiFi.begin(SSID, PASSWD);
 }
 
 void loop()
 {
-
+  if (WiFi.isConnected())
+  {
+    Serial.printf("Connecté");
+    digitalWrite(PIN_OUTPUT, HIGH);
+  };
 }
